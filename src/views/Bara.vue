53ee9379ae4aaf6c2b46b2d5916dd929b624210a
kuwabarbara <nobuaki.kuwabara3@gmail.com>
2024-01-29 03:33:13 +0900

    まだ少しfirebaseを参照する関係ない処理が残っていたから削除した

--- src/views/Bara.vue
+++ src/views/Bara.vue
@@ -40,14 +40,6 @@
 
 import {  getDefaultSession } from '@inrupt/solid-client-authn-browser'
 import { fetch } from '@inrupt/solid-client-authn-browser'
-//import { getSolidDataset, saveSolidDatasetAt } from "@inrupt/solid-client";
-//import { getPodUrlAll } from "@inrupt/solid-client";
-//import { getThingAll, getStringNoLocale } from "@inrupt/solid-client";
-//import { SCHEMA_INRUPT} from "@inrupt/vocab-common-rdf";
-
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import 'firebase/compat/database';
 
   // Import from "@inrupt/solid-client"
   import {
@@ -88,26 +80,6 @@ export default {
     },
     created() {
         this.completeLogin();
-
-
-
-        // 現在認証されているユーザーのUIDを取得する
-        const uid = firebase.auth().currentUser.uid;
-
-        // Realtime Databaseの参照を作成する
-        const dbRef = firebase.database().ref(`users/${uid}`);
-
-        // データを取得する
-        dbRef.once('value').then((snapshot) => {
-            const userData = snapshot.val(); // データをオブジェクト形式で取得する
-            const gender = userData.gender; // genderを取得する
-            const name = userData.name; // nameを取得する
-            const status = userData.status; // statusを取得する
-
-            console.log(gender);
-            console.log(name);
-            console.log(status);
-        });
 
 
         
@@ -244,85 +216,6 @@ export default {
                 });
         },
 
-        /*//リソースへのアクセス権を与える関数（ここではkuwabarbara2にアクセスの許可を与える）
-        async setupPolicyToMatchAgentsAndClients(resourceURL) {
-
-            const agentsToMatch = [ "https://id.inrupt.com/kuwabarbara2" ];
-            const clientIDsToMatch = [ "http://localhost:8080/bara" ];
-
-            try {
-                // 1. Fetch the SolidDataset with its Access Control Resource (ACR).
-                let resourceWithAcr = await acp_ess_2.getSolidDatasetWithAcr(
-                resourceURL,            // Resource whose ACR to set up
-                { fetch: fetch }       // fetch from the authenticated session
-                );
-
-                // 2. Initialize a new Matcher.
-                let appFriendsMatcher = acp_ess_2.createResourceMatcherFor(
-                resourceWithAcr,
-                "match-app-friends"
-                );
-
-                // 3. For the Matcher, specify the Agent(s) to match.
-                agentsToMatch.forEach(agent => {
-                appFriendsMatcher = acp_ess_2.addAgent(appFriendsMatcher, agent);
-                })
-
-                // 4. For the Matcher, specify the Client ID(s) to match.
-                clientIDsToMatch.forEach(clientID => {
-                appFriendsMatcher = acp_ess_2.addClient(appFriendsMatcher, clientID);
-                })
-
-                // 5. Add the Matcher definition to the Resource's ACR.
-                resourceWithAcr = acp_ess_2.setResourceMatcher(
-                resourceWithAcr,
-                appFriendsMatcher
-                );
-
-                // 6. Create a Policy for the Matcher.
-                let appFriendsPolicy = acp_ess_2.createResourcePolicyFor(
-                resourceWithAcr,
-                "app-friends-policy",
-                );
-
-                // 7. Add the appFriendsMatcher to the Policy as an allOf() expression.
-                // Since using allOf() with a single Matcher, could also use anyOf() expression
-
-                appFriendsPolicy = acp_ess_2.addAllOfMatcherUrl(
-                appFriendsPolicy,
-                appFriendsMatcher
-                );
-
-                // 8. Specify the access modes (e.g., allow Read and Write).
-                appFriendsPolicy = acp_ess_2.setAllowModes(appFriendsPolicy,
-                { read: true, write: true }
-                );
-
-                // 9. Apply the Policy to the resource.
-                resourceWithAcr = acp_ess_2.addPolicyUrl(
-                resourceWithAcr,
-                asUrl(appFriendsPolicy)
-                );
-
-                // 10. Add the Policy definition to the resource's ACR. 
-                resourceWithAcr = acp_ess_2.setResourcePolicy(
-                resourceWithAcr,
-                appFriendsPolicy
-                );
-
-                // 11. Save the modified ACR for the resource.
-                const updatedResourceWithAcr = await acp_ess_2.saveAcrFor(
-                resourceWithAcr,
-                { fetch: fetch }          // fetch from the authenticated session
-                );
-
-                console.log("Updated ACR: ", updatedResourceWithAcr);
-
-            } catch (error) {
-                console.error(error.message);
-            }
-        },*/
-
 
 
         async  completeLogin() {
@@ -411,73 +304,6 @@ export default {
             } catch (error) {
             console.log(error);
             }
-
-
-
-
-
-
-            /*console.log(myChangedDataset);
-            console.log(this.PodUrl);
-
-
-
-
-            const readingListUrl = this.PodUrl;
-  
-            let titles = myChangedDataset.value.split("\n");
-
-            // Fetch or create a new reading list.
-            let myReadingList;
-
-            try {
-                // Attempt to retrieve the reading list in case it already exists.
-                myReadingList = await getSolidDataset(readingListUrl, { fetch: fetch });
-                // Clear the list to override the whole list
-                let items = getThingAll(myReadingList);
-                items.forEach((item) => {
-                myReadingList = removeThing(myReadingList, item);
-                });
-            } catch (error) {
-                if (typeof error.statusCode === "number" && error.statusCode === 404) {
-                // if not found, create a new SolidDataset (i.e., the reading list)
-                myReadingList = createSolidDataset();
-                } else {
-                console.error(error.message);
-                }
-            }
-
-            // Add titles to the Dataset
-            let i = 0;
-            titles.forEach((title) => {
-                if (title.trim() !== "") {
-                let item = createThing({ name: "title" + i });
-                item = addUrl(item, RDF.type, AS.Article);
-                item = addStringNoLocale(item, SCHEMA_INRUPT.name, title);
-                myReadingList = setThing(myReadingList, item);
-                i++;
-                }
-            });
-
-
-
-
-
-
-
-            //const URL=this.PodUrl+ "todolist";
-
-            // The user must be someone with Write access to the specified URL.
-            const savedSolidDataset = await saveSolidDatasetAt(
-            //"https://storage.inrupt.com/somepod/todolist",
-            this.PodUrl,
-            //myChangedDataset,
-            myReadingList,
-            { fetch: fetch }
-            );
-            console.log(savedSolidDataset);
-
-            //this.SaveData = savedSolidDataset;*/
         }
 
 
